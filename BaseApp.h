b9837573bdc5602ae787fbd606f8fc413d744c94
CokeU17 <158185862+CokeU17@users.noreply.github.com>
2026-04-27 07:24:42 -0600

    Add files via upload

---
+++ BaseApp.h
@@ -0,0 +1,110 @@
+#pragma once
+#include "Prerequisites.h"
+#include "Window.h"
+#include "Device.h"
+#include "DeviceContext.h"
+#include "SwapChain.h"
+#include "Texture.h"
+#include "RenderTargetView.h"
+#include "DepthStencilView.h"
+#include "Viewport.h"
+#include "ShaderProgram.h"
+#include "MeshComponent.h"
+#include "Buffer.h"
+#include "SamplerState.h"
+#include "Model3D.h"
+#include "ECS/Actor.h"
+#include "EngineUtilities\GUI/GUI.h"
+#include "SceneGraph\SceneGraph.h"
+#include "EngineUtilities\Utilities\Camera.h"
+#include "EngineUtilities\Utilities\Skybox.h"
+#include "EngineUtilities\Utilities\LayoutBuilder.h"
+#include "EngineUtilities/Utilities/EditorViewportPass.h"
+extern IMGUI_IMPL_API
+LRESULT ImGui_ImplWin32_WndProcHandler(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam);
+
+class 
+BaseApp {
+public:
+	BaseApp() = default;
+	~BaseApp() { destroy(); }
+
+	HRESULT
+	awake();
+
+	int 
+	run(HINSTANCE hInst, int nCmdShow);
+	
+	HRESULT
+	init();
+
+	void 
+	update(float deltaTime);
+
+	void 
+	render();
+
+	void 
+	destroy();
+
+	void 
+	onResize(unsigned int newW, unsigned int newH);
+
+	void handleEditorViewportResize();
+private:
+	static LRESULT CALLBACK 
+	WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam);
+
+
+private:
+	Window                              m_window;
+	Device															m_device;
+	DeviceContext												m_deviceContext;
+	SwapChain                           m_swapChain;
+	Texture                             m_backBuffer;
+	RenderTargetView									  m_renderTargetView;
+	Texture                             m_depthStencil;
+	DepthStencilView									  m_depthStencilView;
+	Viewport                            m_viewport;
+	ShaderProgram												m_shaderProgram;
+	//Buffer															m_cbNeverChanges;
+	//Buffer															m_cbChangeOnResize;
+	bool m_d3dReady = false;
+	Buffer m_constantBuffer;
+	CBMain m_constantBufferStruct;
+
+	// Textures
+	Texture m_AlbedoSRV;
+	Texture m_MetallicSRV;
+	Texture m_RoughnessSRV;
+	Texture m_AOSRV;
+	Texture m_NormalSRV;
+
+	Camera															m_camera;
+
+	SceneGraph													m_sceneGraph;
+	std::vector<EU::TSharedPointer<Actor>> m_actors;
+	EU::TSharedPointer<Actor> m_cyberGun;
+
+	
+	Model3D*														m_model;
+
+	//CBChangeOnResize										cbChangesOnResize;
+	//CBNeverChanges											cbNeverChanges;
+	GUI																m_gui;
+	EU::Vector3 m_cameraPos;
+
+	Skybox m_skybox;
+	Texture															m_skyboxTex;
+	RasterizerState m_defaultRasterizer;
+	DepthStencilState m_defaultDepthStencil;
+
+	EditorViewportPass m_editorViewportPass;
+	bool m_editorViewportResizePending = false;
+	unsigned int m_pendingViewportWidth = 1;
+	unsigned int m_pendingViewportHeight = 1;
+
+	unsigned int m_lastRequestedViewportWidth = 1;
+	unsigned int m_lastRequestedViewportHeight = 1;
+	int m_viewportResizeStableFrames = 0;
+};
---
+++ Prerequisites.h
@@ -0,0 +1,128 @@
+#pragma once
+// Librerias STD
+#include <string>
+#include <sstream>
+#include <vector>
+#include <windows.h>
+#include <xnamath.h>
+#include <thread>
+#include <memory>
+#include <unordered_map>
+#include <type_traits>
+#include <array>
+
+// Librerias DirectX
+#include <d3d11.h>
+#include <d3dx11.h>
+#include <d3dcompiler.h>
+#include "Resource.h"
+#include "resource.h"
+
+// Third Party Libraries
+#include "EngineUtilities/Vectors/Vector2.h"
+#include "EngineUtilities/Vectors/Vector3.h"
+#include "EngineUtilities\Memory\TSharedPointer.h"
+#include "EngineUtilities\Memory\TWeakPointer.h"
+#include "EngineUtilities\Memory\TStaticPtr.h"
+#include "EngineUtilities\Memory\TUniquePtr.h"
+
+// MACROS
+#define SAFE_RELEASE(x) if(x != nullptr) x->Release(); x = nullptr;
+
+#define MESSAGE( classObj, method, state )   \
+{                                            \
+   std::wostringstream os_;                  \
+   os_ << classObj << "::" << method << " : " << "[CREATION OF RESOURCE " << ": " << state << "] \n"; \
+   OutputDebugStringW( os_.str().c_str() );  \
+}
+
+#define ERROR(classObj, method, errorMSG)                     \
+{                                                             \
+    try {                                                     \
+        std::wostringstream os_;                              \
+        os_ << L"ERROR : " << classObj << L"::" << method     \
+            << L" : " << errorMSG << L"\n";                   \
+        OutputDebugStringW(os_.str().c_str());                \
+    } catch (...) {                                           \
+        OutputDebugStringW(L"Failed to log error message.\n");\
+    }                                                         \
+}
+
+//--------------------------------------------------------------------------------------
+// Structures
+//--------------------------------------------------------------------------------------
+struct SimpleVertex
+{
+  EU::Vector3 Position;
+  EU::Vector3 Normal;
+  EU::Vector3 Tangent;
+  EU::Vector3 Bitangent;
+  EU::Vector2 TextureCoordinate;
+};
+
+struct 
+SkyboxVertex {
+	float x,y,z;
+};
+
+
+struct CBNeverChanges
+{
+  XMMATRIX mView;
+};
+
+struct CBSkybox
+{
+  XMMATRIX mviewProj;
+};
+
+struct CBChangeOnResize
+{
+  XMMATRIX mProjection;
+};
+
+// Constant buffer used in the vertex and pixel shaders.  Align to
+// 16?bytes as required by Direct3D constant buffers.
+struct CBMain
+{
+  //XMFLOAT4X4 World;
+  XMFLOAT4X4 View;
+  XMFLOAT4X4 Projection;
+  EU::Vector3 CameraPos;
+  float pad0;
+  EU::Vector3 LightDir;
+  float pad1;
+  EU::Vector3 LightColor;
+  float pad2;
+};
+
+struct CBChangesEveryFrame
+{
+  XMMATRIX mWorld;
+  XMFLOAT4 vMeshColor;
+};
+
+enum ExtensionType {
+  DDS = 0,
+  PNG = 1,
+  JPG = 2
+};
+
+enum ShaderType {
+  VERTEX_SHADER = 0,
+  PIXEL_SHADER = 1
+};
+
+/**
+ * @enum ComponentType
+ * @brief Tipos de componentes disponibles en el juego.
+ */
+enum 
+ComponentType {
+  NONE = 0,     ///< Tipo de componente no especificado.
+  TRANSFORM = 1,///< Componente de transformación.
+  MESH = 2,     ///< Componente de malla.
+  MATERIAL = 3,  ///< Componente de material.
+	HIERARCHY = 4 ///< Componente de jerarquía.
+};
+
